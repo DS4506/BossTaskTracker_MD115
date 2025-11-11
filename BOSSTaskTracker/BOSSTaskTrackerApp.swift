c295fd1831bf67a1cb3df5324161b8efca137ed6
DS4506 <davidsmith4506@gmail.com>
2025-11-10 21:27:52 -0600

    Initial Commit

---
+++ BOSSTaskTracker/BOSSTaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOSSTaskTrackerApp.swift
+//  BOSSTaskTracker
+//
+//  Created by Willie Earl on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BOSSTaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
